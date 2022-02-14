c2a55f6d39d9b86f98b7a55e364d034c2416b9b8
arylise <arylise.j@gmail.com>
2022-02-15 01:36:33 +0800

    commit

---
+++ src/test/java/com/assistant/AssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
